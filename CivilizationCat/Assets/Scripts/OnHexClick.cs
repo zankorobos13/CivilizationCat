454315d09f2dc5c9c0354cd3ea102e93e0d8059c
zankorobos13 <aruseniys13@yandex.ru>
2023-08-25 23:20:05 +0300

    Добавлена возможность получения координат нажатой клетки
    При нажатию на клетку на карте в консоль выводятся её координаты, в дальнейшем будет использоваться для действий на конкретные клетки

--- CivilizationCat/Assets/Scripts/OnHexClick.cs
+++ CivilizationCat/Assets/Scripts/OnHexClick.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class OnHexClick : MonoBehaviour
 {
+    private GameObject[,] _fields;
     void Start()
     {
-        
+        _fields = MapGenerationScript.fields;
     }
 
     private void OnMouseDown()
     {
-        Debug.Log(123);
+        for (int i = 0; i < _fields.GetLength(0); i++)
+            for (int j = 0; j < _fields.GetLength(1); j++)
+                if (_fields[i, j].transform.position == gameObject.transform.position)
+                    Debug.Log(i + " " + j);
     }
 }
