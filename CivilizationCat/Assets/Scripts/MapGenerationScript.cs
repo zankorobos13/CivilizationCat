3b74146124fdfca10b008f75911cc5e511cf106a
zankorobos13 <aruseniys13@yandex.ru>
2023-08-25 23:14:50 +0300

    Добавлена генерация карты
    Добавлена генерация карты с заданным размером

---
+++ CivilizationCat/Assets/Scripts/MapGenerationScript.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerationScript : MonoBehaviour
+{
+    public GameObject map;
+    public GameObject field;
+    public static GameObject[,] fields;
+    public int len;
+    private float _hex_size_default_x = 1;
+    private float _hex_size_default_y = 1;
+    private float _map_size_x = 8;
+    private float _map_size_y = 8;
+    public float offset;
+
+
+    void Start()
+    {
+        fields = new GameObject[len, len];
+
+        field.transform.localScale = new Vector3(((_map_size_x - ((len - 1) * offset)) / len) * _hex_size_default_x, ((_map_size_y - ((len - 1) * offset)) / len) * _hex_size_default_y, field.transform.localScale.z);
+
+        int i = 0;
+        int j = 0;
+
+        for (float x = 0; x < _map_size_x; x += field.transform.localScale.x + offset)
+        {
+            for (float y = 0; y < _map_size_y; y += field.transform.localScale.y + offset)
+            {
+                GameObject new_field;
+                float nY = y + ((field.transform.localScale.y / 4) * (len - j));
+
+                if (j % 2 == 1)
+                {
+                    new_field = Instantiate(field, new Vector3(x, nY, 0), Quaternion.identity);
+                }
+                else
+                {
+                    new_field = Instantiate(field, new Vector3(x + (field.transform.localScale.x * field.transform.Find("FieldSprite").localScale.x / 2), nY, 0), Quaternion.identity); 
+                }
+               
+
+                new_field.transform.parent = map.transform;
+                fields[i, j] = new_field;
+                j++;
+            }
+            i++;
+            j = 0;
+        }
+
+        map.transform.position = new Vector2(-8.5f, -4f);
+    }
+
+}
---
+++ CivilizationCat/Assets/Scripts/OnHexClick.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnHexClick : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log(123);
+    }
+}
